621bbcc116ef6eb73c64d1deee52b39d9478f734
LeeJiEuns <87702194+LeeJiEuns@users.noreply.github.com>
2025-03-13 14:22:37 +0900

    Feat : BtnIco.jsx 코드 수정

--- src/components/buttons/BtnIco.jsx
+++ src/components/buttons/BtnIco.jsx
@@ -1,8 +1,8 @@
 import { BtnStyled } from "./btn.styles";
 
-const BtnIco = ({ variant, text = "충전하기", icon = "ic-check.svg", onClick }) => {
+const BtnIco = ({ disabled = false, text = "충전하기", icon = "ic-check.svg", onClick, ...rest }) => {
     return (
-        <BtnStyled variant={variant} onClick={onClick}>
+        <BtnStyled disabled={disabled} onClick={onClick} {...rest}>
             <img src={`../src/assets/icons/${icon}`} alt="" width={20} />
             {text}
         </BtnStyled>
