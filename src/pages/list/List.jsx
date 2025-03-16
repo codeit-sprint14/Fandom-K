98350ff9fe9f839794355fb29aefe1d85001df77
huiseong29 <tobestar29@naver.com>
2025-03-17 01:25:58 +0900

    List.jsx 파일 불필요한 코드 일부 삭제

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,22 +1,8 @@
-// window.localStorage.setItem("credit", 30000);
-
-// function List() {
-//   return <>List</>;
-// }
-
-// export default List;
-
-import { ThemeProvider } from "styled-components";
-import theme from "../../styles/theme.styles";
 import DonationPage from "./donation/components/DonationPage";
 window.localStorage.setItem("credit", 30000);
 
 function List() {
-  return (
-    <ThemeProvider theme={theme}>
-      <DonationPage />
-    </ThemeProvider>
-  );
+  return <DonationPage />;
 }
 
 export default List;
